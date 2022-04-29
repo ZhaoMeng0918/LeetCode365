ec2b16d42565ca2d328b812a713dc749242f54aa
ZhaoMeng0918 <15991808959@163.com>
2022-04-29 22:16:03 +0800

    完全背包太难啦！！！

---
+++ Projects/LeetCode-DynamicPrograming/src/com/leetcode/dp/LeetCode_139_单词拆分.java
@@ -0,0 +1,19 @@
+package com.leetcode.dp;
+
+import java.util.List;
+
+public class LeetCode_139_单词拆分 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] valid = new boolean[s.length() + 1];
+        valid[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (wordDict.contains(s.substring(j, i)) && valid[j]) {
+                    valid[i] = true;
+                }
+            }
+        }
+
+        return valid[s.length()];
+    }
+}
