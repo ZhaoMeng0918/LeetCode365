ac36f3671a4def41ba04fc5b98a4dfe8afe63aa5
GerryZhao <15991808959@163.com>
2022-05-10 22:02:36 +0800

    代码随想录1刷

--- DaiMaSuiXiangLu_1/src/com/leetcode/LeetCode_数组_209_长度最小的子数组.java
+++ DaiMaSuiXiangLu_1/src/com/leetcode/LeetCode_数组_209_长度最小的子数组.java
@@ -1,11 +1,50 @@
 package com.leetcode;
 
-/**
- * @ClassName LeetCode_数组_209_长度最小的子数组
- * @Description TODO
- * @Author Administrator
- * @Date 2022-05-08 16:00
- * @Version 1.0.0
- */
 public class LeetCode_数组_209_长度最小的子数组 {
+    /**
+     * 暴力方法
+     *
+     * @param target
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen_1(int target, int[] nums) {
+        int result = Integer.MAX_VALUE;
+        int sum = 0;
+        int subLength = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum >= target) {
+                    subLength = j - i + 1;
+                    result = result < subLength ? result : subLength;
+                    break;
+                }
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+
+    /**
+     * 滑动窗口
+     *
+     * @param target
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen_2(int target, int[] nums) {
+        int left = 0;
+        int right = 0;
+        int result = Integer.MAX_VALUE;
+        int sum = 0;
+        for (right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum >= target) {
+                result = Math.min(result, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
 }
