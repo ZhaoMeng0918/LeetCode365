3613cbf9b6059e83b93a5a78fff2ad78b2d4edaf
GerryZhao <15991808959@163.com>
2022-05-15 11:56:58 +0800

    数组 滑动窗口

--- DaiMaSuiXiangLu_1/src/com/leetcode/LeetCode_数组_904_水果成篮.java
+++ DaiMaSuiXiangLu_1/src/com/leetcode/LeetCode_数组_904_水果成篮.java
@@ -1,11 +1,61 @@
 package com.leetcode;
 
-/**
- * @ClassName LeetCode_数组_904_水果成篮
- * @Description TODO
- * @Author Administrator
- * @Date 2022-05-08 16:00
- * @Version 1.0.0
- */
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
+import java.util.*;
+
 public class LeetCode_数组_904_水果成篮 {
+    /**
+     * 暴力破解
+     *
+     * @param fruits
+     * @return
+     */
+    public int totalFruit_1(int[] fruits) {
+        int result = Integer.MIN_VALUE;
+        int i = 0;
+        int j = 0;
+        HashSet<Integer> types = new HashSet<>();
+        int count = 0;
+        for (i = 0; i < fruits.length && j != fruits.length; i++) {
+            count = 0;
+            types.clear();
+            for (j = i; j < fruits.length; j++) {
+                if (types.size() < 2 || types.contains(fruits[j])) {
+                    types.add(fruits[j]);
+                    count++;
+                } else {
+                    result = Math.max(result, count);
+                    break;
+                }
+
+            }
+        }
+        return Math.max(result, count);
+    }
+
+    /**
+     * 滑动窗口
+     *
+     * @param fruits
+     * @return
+     */
+    public int totalFruit_2(int[] fruits) {
+        int n = fruits.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+            while (map.size() > 2) {
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+                if (map.get(fruits[left]) == 0) {
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
 }
